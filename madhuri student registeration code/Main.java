e7928c78a906521e7441a3f91479b2a559db56d0
madhuridnaik <32697048+madhuridnaik@users.noreply.github.com>
2017-10-30 06:51:08 +0000

    Add files via upload

---
+++ madhuri student registeration code/Main.java
@@ -0,0 +1,16 @@
+package register;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hi");
+		College c = new College();
+		c.display();
+		Course c1 = new Course();
+		c1.displayc();
+	}
+
+}
